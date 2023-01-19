d0a67e6d190b1ba306b5b9208a2ee922a8019854
Anup kumar dutta <anupdutta99v@gmail.com>
2023-01-19 23:11:18 +0530

    direct url

--- controller/image-controller.js
+++ controller/image-controller.js
@@ -21,7 +21,7 @@ export const uploadImage = (request, response) => {
     if(!request.file) 
         return response.status(404).json("File not found");
     
-    const imageUrl = `${url}/file/${request.file.filename}`;
+    const imageUrl = `https://coder-blog-mern-api.onrender.com/file/${request.file.filename}`;
 
     response.status(200).json(imageUrl);    
 }
