f044243d6b2be3635af42bc0e3d2e7f2b2c8a38a
Anup kumar dutta <anupdutta99v@gmail.com>
2023-01-19 20:28:31 +0530

    first commit

--- server.js
+++ server.js
@@ -12,15 +12,17 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin:["http://localhost:3000","https://coder-blog-mern.onrender.com"]
+}));
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', Router);
 
-if(process.env.NODE_ENV === 'production')
-{
-    app.use(express.static("client/build"));
-}
+// if(process.env.NODE_ENV === 'production')
+// {
+//     app.use(express.static("client/build"));
+// }
 
 const PORT = process.env.PORT || 8000;
 const username = process.env.DB_USERNAME;
